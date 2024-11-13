e5e34a12bd3984504f91489cbab24a241503dbd1
Roman <realasdfga@gmail.com>
2024-11-13 20:43:36 +0200

    Add CORSMiddleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.menu import router as menu_router
 
 def create_app():
     app = FastAPI(root_path='/api/v1')
     app.include_router(menu_router)
+
+    origins = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     return app
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 from app import create_app
 
 if __name__ == '__main__':
-    uvicorn.run(create_app(), host='localhost', port=8000)
+    uvicorn.run(create_app(), host='localhost', port=8000, reload=True)
