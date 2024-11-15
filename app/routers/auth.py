88d2b1d720d5686fc755b645a48fc45810273df3
Roman <realasdfga@gmail.com>
2024-11-15 18:16:50 +0200

    Add jwt token refresh

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,10 +3,10 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from app.auth.auth import get_user_by_username, create_jwt_token, get_password_hash, verify_password
+from app.auth.auth import get_user_by_username, create_jwt_token, get_password_hash, verify_password, verify_token
 from app.database import get_async_session
 from app.models.users import User
-from app.schemas.auth import SUserLogin, SUserRegister, SUserLoginResponse
+from app.schemas.auth import SUserLogin, SUserRegister, SUserLoginResponse, SToken
 from app.schemas.users import SUserResponse
 
 router = APIRouter(
@@ -43,3 +43,14 @@ async def login(user_data: SUserLogin = Depends(), session: AsyncSession = Depen
     response.refresh_token = refresh_token
     response.token_type = 'Bearer'
     return response
+
+
+@router.post("/token/refresh")
+async def token_refresh(refresh_token: str | None = None):
+    payload = await verify_token(refresh_token, "refresh")
+    user_username: str = payload.get("sub")
+    if user_username is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    new_access_token = await create_jwt_token(data={"sub": user_username}, token_type='access')
+    new_refresh_token = await create_jwt_token(data={"sub": user_username}, token_type='refresh')
+    return SToken(access_token=new_access_token, refresh_token=new_refresh_token, token_type="Bearer")
--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -12,7 +12,13 @@ class SUserLogin(BaseModel):
     password: str
 
 
-class SUserLoginResponse(SUserResponse):
+class SToken(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class SUserLoginResponse(SToken, SUserResponse):
     access_token: str | None = None
     refresh_token: str | None = None
     token_type: str | None = None
