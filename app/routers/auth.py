786e32ade643619b19f30153420378a1aeac901b
Roman <realasdfga@gmail.com>
2025-01-26 19:57:57 +0200

    Changes in jwt tokens and login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -31,12 +31,10 @@ async def register(user: SUserRegister = Depends(), session: AsyncSession = Depe
 
 
 @router.post("/login")
-async def login(user_data: SUserLogin = Depends(), session: AsyncSession = Depends(get_async_session)):
+async def login(user_data: SUserLogin, session: AsyncSession = Depends(get_async_session)):
     user = await get_user_by_username(user_data.username, session)
-    if not user:
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
-    if not verify_password(user_data.password, user.password):
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+    if not user or not verify_password(user_data.password, user.password):
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Invalid username or password")
     access_token = await create_jwt_token(data={"sub": user.username}, token_type='access')
     refresh_token = await create_jwt_token(data={"sub": user.username}, token_type='refresh')
     await session.commit()
--- app/services/auth.py
+++ app/services/auth.py
@@ -27,16 +27,19 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 async def create_jwt_token(data: dict, token_type: str = 'access', expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(settings.refresh_token_expires_days))
+    if token_type == 'access':
+        expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.access_token_expires_minutes))
+    else:
+        expire = datetime.now(timezone.utc) + (expires_delta or timedelta(days=settings.refresh_token_expires_days))
     to_encode.update({'exp': expire, 'type': f'{token_type}'})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.jwt_algorithm)
     return encoded_jwt
 
 
-async def verify_token(token: str, token_type: str):
+async def verify_token(token: str, token_type: str | None = None):
     try:
         payload = jwt.decode(token, settings.secret_key, algorithms=[settings.jwt_algorithm])
-        if payload.get('type') != token_type:
+        if token_type and payload.get('type') != token_type:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token type")
         return payload
     except jwt.ExpiredSignatureError:
