5b03f10025a91aea84f0f826fb9baa0841a82540
Roman <realasdfga@gmail.com>
2025-02-08 16:32:22 +0200

    Auth to connect to ws

--- app/services/websockets.py
+++ app/services/websockets.py
@@ -1,9 +1,14 @@
 import json
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Query, Depends, HTTPException
 from typing import List, Dict
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from app.database import get_async_session
 from app.schemas.orders import SOrderResponse, STableResponse, SOrderItemResponse
+from app.services.auth import get_current_user
 
 router = APIRouter()
 
@@ -38,18 +43,34 @@ async def handle_websocket(websocket: WebSocket, connection_type: str, order_id:
 
 
 @router.websocket("/ws/orders")
-async def websocket_orders(websocket: WebSocket):
-    await handle_websocket(websocket, "orders")
+async def websocket_orders(websocket: WebSocket, token: str = Query(...),
+                           session: AsyncSession = Depends(get_async_session)):
+    print(active_connections)
+    try:
+        await get_current_user(session, token)
+        await handle_websocket(websocket, "orders")
+    except HTTPException:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
 
 
 @router.websocket("/ws/orders/{order_id}")
-async def websocket_order_detail(websocket: WebSocket, order_id: int):
-    await handle_websocket(websocket, "orders", order_id=order_id)
+async def websocket_order_detail(websocket: WebSocket, order_id: int, token: str = Query(...),
+                                 session: AsyncSession = Depends(get_async_session)):
+    try:
+        await get_current_user(session, token)
+        await handle_websocket(websocket, "orders", order_id=order_id)
+    except HTTPException:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
 
 
 @router.websocket("/ws/tables")
-async def websocket_tables(websocket: WebSocket):
-    await handle_websocket(websocket, "tables")
+async def websocket_tables(websocket: WebSocket, token: str = Query(...),
+                           session: AsyncSession = Depends(get_async_session)):
+    try:
+        await get_current_user(session, token)
+        await handle_websocket(websocket, "tables")
+    except HTTPException:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
 
 
 async def broadcast_update(connection_type: str, data):
